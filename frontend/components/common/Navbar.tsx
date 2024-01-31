135996cce7c3aaf6a3feaa531ab281c39138bd33
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-01-31 09:59:28 +0400

    update all

--- frontend/components/common/Navbar.tsx
+++ frontend/components/common/Navbar.tsx
@@ -95,7 +95,8 @@ export default function Navbar() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
+					
+					  <Menu.Item>
                         {({ active }) => (
                           <a
                             href="#"
@@ -173,6 +174,9 @@ export default function Navbar() {
 							</div>
 						</div>
 					</div>
+					
+
+					
 
 					<Disclosure.Panel className='sm:hidden'>
 						<div className='space-y-1 px-2 pb-3 pt-2'>
